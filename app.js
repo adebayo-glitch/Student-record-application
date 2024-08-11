8891e57a5018e51b4a9a4dc0264cf7796acdbb1b
adebayo-glitch <pillarbasetech@gmail.com>
2024-08-11 10:58:19 -0400

    Add event listener for submit

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
 // Cache elements by getElementById 
 const studentForm = document.getElementById('addStudentForm');
+
+// Cache elements by querySelector 
+const studentList = document.querySelector('#students');
+const studentTemplate = document.querySelector('#studentTemplate');
+
+// Event listeners
+studentForm.addEventListener('submit', addStudent);
